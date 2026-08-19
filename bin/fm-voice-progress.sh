292d14c361f1abe5ff0ca3950cccda8ccf1209eb
Marcus Nascimento <marcus.nascimento@gmail.com>
2026-08-19 13:27:53 -0400

    no-mistakes(document): Add comment explaining C0 control-byte JSON-escape safety invariant

--- bin/fm-voice-progress.sh
+++ bin/fm-voice-progress.sh
@@ -86,6 +86,8 @@ fm_voice_json_escape() {  # <text> <result-var>
   text=${text//$'\n'/ }
   text=${text//$'\t'/ }
   text=${text//$'\r'/ }
+  # JSON requires every C0 control byte to be escaped or absent; \n \t \r are
+  # normalized above, so strip the remaining unescaped C0 range here.
   text=$(printf '%s' "$text" | LC_ALL=C tr -d '\001-\010\013\014\016-\037')
   printf -v "$result_var" '%s' "$text"
 }
